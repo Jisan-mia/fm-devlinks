cee1a4e8bcf5535ac00b07cb1c5f268dfcbbbb78
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-10-11 17:01:34 +0600

    feat: shareable devlinks profile with dynamic routing

---
+++ app/[id]/page.tsx
@@ -0,0 +1,14 @@
+import PreviewProfileCard from "@/components/preview-profile/preview-profile-card";
+import PreviewProfileCover from "@/components/preview-profile/preview-profile-cover";
+
+const DevLinkIndividualProfile = () => {
+  return (
+    <main className="relative">
+      <PreviewProfileCover />
+
+      <PreviewProfileCard className="mt-20" />
+    </main>
+  );
+};
+
+export default DevLinkIndividualProfile;
--- components/preview-profile/preview-profile-card.tsx
+++ components/preview-profile/preview-profile-card.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { DevLinkProfileType } from "@/types/devlinks";
 import ProfileCard from "../profile-card/profile-card";
 
@@ -34,10 +35,19 @@ const exampleProfile: DevLinkProfileType = {
     },
   ],
 };
-const PreviewProfileCard = () => {
+
+interface IPreviewProfileCardProps {
+  className?: string;
+}
+const PreviewProfileCard = ({ className }: IPreviewProfileCardProps) => {
   return (
     <div className="w-full h-full container flex justify-center items-center">
-      <div className="w-[400px]  bg-white rounded-2xl shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] min-h-[calc(100vh-180px)] mt-8 mobile-sm:w-full">
+      <div
+        className={cn(
+          "w-[400px]  bg-white rounded-2xl shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] min-h-[calc(100vh-180px)] mt-8 mobile-sm:w-full",
+          className
+        )}
+      >
         <ProfileCard profile={exampleProfile}>
           <ProfileCard.Detail>
             <ProfileCard.Image />
